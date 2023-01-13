a0829ff710d92349e0fe9043142f29a467b8d7a3
Hamzaansari15 <hamzaansaree15@gmail.com>
2023-01-14 03:58:02 +0500

    Update App.js

--- PizzaApp/App.js
+++ PizzaApp/App.js
@@ -1,18 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
 import StackNavigation from './screens/StackNavigation'
 
 const App = () => {
 
 
-
-  // useEffect(() => {
-  //   fetch('https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=c6f60a4ad4d8ef31bf998fb79ebd1ae1&mode=HTML')
-  // .then(response => response.json())
-  // .then(response => {console.log(response)
-  // })
-  // .catch(err => console.error(err));
-  // })
 
   return (
       <StackNavigation />
