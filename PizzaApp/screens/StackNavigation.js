10fc6b95a8f11835b02a6820eb5eebdc696c45d8
Hamzaansari15 <hamzaansaree15@gmail.com>
2023-01-14 03:00:43 +0500

    Update StackNavigation.js

--- PizzaApp/screens/StackNavigation.js
+++ PizzaApp/screens/StackNavigation.js
@@ -1,25 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './MainScreen';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-
-const Drawer = createDrawerNavigator();
+import SecondScreen from './SecondScreen';
+import ThirdScreen from './ThirdScreen';
+import ForthScreen from './ForthScreen';
 
 const Stack = createNativeStackNavigator();
 
+
 const StackNavigation = () => {
-    return (
-        <NavigationContainer>
-            <Drawer.Navigator initialRouteName="Home">
-                <Drawer.Screen name="Home" component={HomeScreen} />
-            </Drawer.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name='hhh' component={MainScreen} options={{headerShown: false}} />
+            <Stack.Screen name="SecondScreen" component={SecondScreen} options={{title: 'Uncle Jhon Pizzas'}} />
+            <Stack.Screen name="ThirdScreen" component={ThirdScreen} options={{title: 'Uncle Jhon Pizzas'}} />
+            <Stack.Screen name="ForthScreen" component={ForthScreen} options={{title: 'Uncle Jhon Pizzas'}} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default StackNavigation
 
 const styles = StyleSheet.create({})
+
