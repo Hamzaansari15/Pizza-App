cba463cb5750c98e80fb1b38432f78606f48063f
Hamzaansari15 <hamzaansaree15@gmail.com>
2023-01-14 03:00:25 +0500

    Create Drawer.js

---
+++ PizzaApp/screens/Drawer.js
@@ -0,0 +1,16 @@
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+
+// const Drawer = createDrawerNavigator();
+
+// const MyDrawer = () => {
+//   return (
+//     <View></View>
+//   )
+// }
+
+// export default MyDrawer
+
+// const styles = StyleSheet.create({})
